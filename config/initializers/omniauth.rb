de417ea30535ae2298372820fed505c274abbea6
Lisa Huang <lisa.yc.huang@gmail.com>
2018-09-23 18:07:10 -0700

    Added OAuth scopes to get permissions for Github

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,4 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :github, ENV['OMNIAUTH_PROVIDER_KEY'], ENV['OMNIAUTH_PROVIDER_SECRET']
+  provider :github, ENV['OMNIAUTH_PROVIDER_KEY'], ENV['OMNIAUTH_PROVIDER_SECRET'], 
+                    scope: "read:org,notifications,user:email,repo"
 end

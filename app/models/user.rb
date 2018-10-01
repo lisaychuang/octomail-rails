45029a26ec7dcffbfc5868b5c17332ef893f1270
Lisa Huang <lisa.yc.huang@gmail.com>
2018-09-30 17:02:57 -0700

    Token encryption with MessageEncryptor

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,25 +3,47 @@ class User < ApplicationRecord
   validates :uid, uniqueness: true
   # attr_encrypted :token, key: ENV["GH_TOKEN_KEY"]
 
+  def token
+    # decrypting github_token using MessageEncryptor
+    key = ENV["GH_TOKEN_KEY"]
+    crypt = ActiveSupport::MessageEncryptor.new(key[0..31])
+    crypt.decrypt_and_verify(self.encrypted_token)
+  end
+
+  # Creating new user upon authentication
+  # Should handle update info at some point
+
   def self.create_with_omniauth(auth)
     create! do |user|
-      user.provider = auth["provider"]
-      user.uid = auth["uid"]
-      if auth["info"]
-        user.name = auth["info"]["name"] || ""
-        user.email = auth["info"]["email"]
-        user.username = auth["info"]["nickname"]
-      end
-      if auth["credentials"]
-        # token received from Github payload
-        github_token = auth["credentials"]["token"]
-
-        # encrypting github_token using MessageEncryptor
-        key = ENV["GH_TOKEN_KEY"]
-        crypt = ActiveSupport::MessageEncryptor.new(key)
-        user.encrypted_token = crypt.encrypt_and_sign(github_token)
-        
-      end
+      set_omniauth_info(user, auth)
     end
   end
+
+  def update_omniauth_info(auth)
+    User.set_omniauth_info(self, auth)
+    self.save
+  end
+
+  private
+  # Class method
+  def self.set_omniauth_info(user, auth) 
+    user.provider = auth["provider"]
+    user.uid = auth["uid"]
+    if auth["info"]
+      user.name = auth["info"]["name"] || ""
+      user.email = auth["info"]["email"]
+      user.username = auth["info"]["nickname"]
+    end
+    if auth["credentials"]
+      # token received from Github payload
+      github_token = auth["credentials"]["token"]
+
+      # encrypting github_token using MessageEncryptor
+      key = ENV["GH_TOKEN_KEY"]
+      crypt = ActiveSupport::MessageEncryptor.new(key[0..31])
+      user.encrypted_token = crypt.encrypt_and_sign(github_token)
+      
+    end
+  end
+
 end

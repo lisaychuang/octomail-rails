aa4d97fb18927ab7daf18645b48643d47dd35e43
Lisa Huang <lisa.yc.huang@gmail.com>
2018-10-03 14:01:26 -0700

    Fixed user login failure handling flow in sessions controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -15,17 +15,24 @@ class SessionsController < ApplicationController
     # If the user exists, update it with any useful info we got from github
     if existing_user
       existing_user.update_omniauth_info(auth)
-    else
-      redirect_to '/auth/failure'
     end
 
     # If existing_user is not defined (i.e., first login), create a new user from github info
     user = existing_user || User.create_with_omniauth(auth)
 
     reset_session
-    session[:user_id] = user.id
-    flash[:notice] = "You have signed in!"
-    redirect_to "https://gitmailz.herokuapp.com/account"
+
+    # If we successfully found existing user or created a new user
+    # Redirect to account info page
+    if user 
+      session[:user_id] = user.id
+      flash[:notice] = "You are signed in!"
+      redirect_to "https://gitmailz.herokuapp.com/account"
+
+    # If not, redirect to failure page and ask user to login again
+    else
+      redirect_to '/auth/failure'
+    end
   end
 
   def destroy

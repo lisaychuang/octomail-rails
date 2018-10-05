ed7b09bc4c2dca10b522c0bc83e47f33f1adf377
Lisa Huang <lisa.yc.huang@gmail.com>
2018-10-04 23:08:37 -0700

    Add private methods to determine notification_url

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -111,5 +111,81 @@ class ApplicationController < ActionController::Base
     end
   end
 
-  
+  # transform api_url for each notification's "subject.type"
+
+  def transform_api_url(subject_type, api_url, title, html_url)
+    case subject_type
+      when "PullRequest"
+        pull_request_url(api_url)
+      when "Release"
+        release_url(api_url, title)
+      when "Issue"
+        issue_url(api_url)
+      when "RepositoryInvitation"
+        invite_url(html_url)
+      else
+        "https://github.com/notifications"
+      end
+  end
+
+  # Notification type: PULL REQUEST
+  # Transform api url to html url
+  def pull_request_url(api_url)
+    # https://api.github.com/repos/typed-ember/ember-cli-typescript/pulls/334
+    api_index = api_url.index("api")
+    repos_index = api_url.index("repos")
+    pulls_index = api_url.index("pulls")
+
+    # https://github.com/typed-ember/ember-cli-typescript/pull/334
+    html_url = api_url[0..api_index - 1] +
+               api_url[api_index + "api.".length..repos_index - 1] +
+               api_url[repos_index + "repos/".length..pulls_index + "pull".length - 1] +
+               api_url[pulls_index + "pulls".length..-1]
+    html_url
+  end
+
+  # Notification type: RELEASE
+  # Transform api url to html url
+  def release_url(api_url, title)
+    # "subject": {
+    #   "title": "v1.4.4",
+    #   "url": "https://api.github.com/repos/typed-ember/ember-cli-typescript/releases/13191777",
+    #   "latest_comment_url": "https://api.github.com/repos/typed-ember/ember-cli-typescript/releases/13191777",
+    #   "type": "Release"
+    # },
+    api_index = api_url.index("api")
+    repos_index = api_url.index("repos")
+    releases_index = api_url.index("releases")
+
+    # https://github.com/typed-ember/ember-cli-typescript/releases/tag/v1.4.4
+    html_url = api_url[0..api_index - 1] +
+               api_url[api_index + "api.".length..repos_index - 1] +
+               api_url[repos_index + "repos/".length..releases_index + "releases".length] +
+               "tag/#{title}"
+    html_url
+  end
+
+  # Notification type: ISSUE
+  # Transform api url to html url
+  def issue_url(api_url)
+    # "https://api.github.com/repos/typed-ember/ember-cli-typescript/issues/322"
+    api_index = api_url.index("api")
+    repos_index = api_url.index("repos")
+
+    # https://github.com/typed-ember/ember-cli-typescript/issues/322
+    html_url = api_url[0..api_index - 1] +
+               api_url[api_index + "api.".length..repos_index - 1] +
+               api_url[repos_index + "repos/".length..-1]
+    html_url
+  end
+
+  # Notification type: REPO INVITATION
+  # Transform repo url to html url
+  def invite_url(html_url)
+    # https://github.com/mike-north/micro-observable
+
+    # https://github.com/mike-north/micro-observable/invitations
+    invite_url = html_url + "/invitations"
+    invite_url
+  end
 end
